0872a2924a2db284cd69e6f385e535917dc5fd0f
L <dzxlzy@gmail.com>
2023-04-06 21:36:40 +0800

    first commit

---
+++ diy-part1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
