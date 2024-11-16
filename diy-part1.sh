6d284828287dcad8269cdcde6ce07f4fdf8ef6b9
dzikaros <tuxttys@gmail.com>
2024-11-17 00:39:43 +0800

    update config

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,3 +7,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+
+# homeproxy
+git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -4,8 +4,3 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 
-# ==========openclash===========
-cd package
-git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
-cd OpenClash
-git sparse-checkout set luci-app-openclash
