c3a9dfd8b31a19c8a7d99767a53cc4793def01ba
dzikaros <tuxttys@gmail.com>
2024-11-25 22:58:35 +0800

    modify default ip

--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -3,4 +3,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
-
+sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/luci2/bin/config_generate
